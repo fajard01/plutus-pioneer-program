d530bf4b78edbfaea1687fa2af9685da04f5c259
Danny Fajardo <fajardodanny@yahoo.co.uk>
2023-12-08 21:12:07 +0000

    week03 homeworks completed

--- code/Week03/homework/Homework1.hs
+++ code/Week03/homework/Homework1.hs
@@ -7,12 +7,16 @@
 
 module Homework1 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash,
-                                       ScriptContext, Validator,
-                                       mkValidatorScript)
-import           PlutusTx             (compile, unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool (..))
-import           Utilities            (wrapValidator)
+import           Plutus.V1.Ledger.Interval  (contains, from, to)
+import           Plutus.V2.Ledger.Api       (BuiltinData, POSIXTime, PubKeyHash,
+                                             ScriptContext (scriptContextTxInfo), 
+                                             TxInfo (txInfoValidRange),
+                                             Validator,
+                                             mkValidatorScript)
+import           Plutus.V2.Ledger.Contexts  (txSignedBy)
+import           PlutusTx                   (compile, unstableMakeIsData)
+import           PlutusTx.Prelude           (Bool (..), ($), (&&), (||), (+), traceIfFalse)
+import           Utilities                  (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
@@ -29,8 +33,32 @@ unstableMakeIsData ''VestingDatum
 -- This should validate if either beneficiary1 has signed the transaction and the current slot is before or at the deadline
 -- or if beneficiary2 has signed the transaction and the deadline has passed.
 mkVestingValidator :: VestingDatum -> () -> ScriptContext -> Bool
-mkVestingValidator _dat () _ctx = False -- FIX ME!
+mkVestingValidator _dat () _ctx = 
+    ( 
+      traceIfFalse "beneficiary2 signature missing" signedByBeneficiary1 &&
+      traceIfFalse "deadline has passed" onOrBeforeDeadline
+    ) ||
+    (
+      traceIfFalse "beneficiary2 signature missing" signedByBeneficiary2 &&
+      traceIfFalse "deadline not yet reached" afterDeadline
+    )
+       where
+          info :: TxInfo
+          info = scriptContextTxInfo _ctx
 
+          signedByBeneficiary1 :: Bool
+          signedByBeneficiary1 = txSignedBy info $ beneficiary1 _dat
+
+          signedByBeneficiary2 :: Bool
+          signedByBeneficiary2 = txSignedBy info $ beneficiary2 _dat
+
+          onOrBeforeDeadline :: Bool
+          onOrBeforeDeadline = contains (to $ deadline _dat) $ txInfoValidRange info
+
+          afterDeadline :: Bool 
+          afterDeadline = contains (from $ (deadline _dat) + 1) $ txInfoValidRange info
+
+          
 {-# INLINABLE  mkWrappedVestingValidator #-}
 mkWrappedVestingValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkWrappedVestingValidator = wrapValidator mkVestingValidator
